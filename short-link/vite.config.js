04bc09d0d1967f78275c0dc958aca9c391a718f1
DuckBurnIncense <DuckBurnIncense@outlook.com>
2022-08-13 04:47:48 +0800

    feat: 配置开发代理服务器

--- short-link/vite.config.js
+++ short-link/vite.config.js
@@ -5,10 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        port: 23456,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:23456/",
+                rewrite: (path) => path.replace(/^\/api/, '')
+            },
+        }
     }
-  }
 })
