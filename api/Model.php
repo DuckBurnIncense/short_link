a15ff84bc29ed4e29e6526d11a1ed077f78e8760
DuckBurnIncense <DuckBurnIncense@outlook.com>
2022-08-14 02:56:34 +0800

    feat(p): 支持封禁链接

--- api/Model.php
+++ api/Model.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/DAO.php';
 
 class Model {
 	public static function get($suffix) {
-		$pp = DAO::new()->pp("SELECT `link` FROM `TN` WHERE `suffix` = ?;");
+		$pp = DAO::new()->pp("SELECT `link` FROM `TN` WHERE `suffix` = ? AND `ban` = 0;");
 		$pp->bv(1, $suffix);
 		$pp->exec();
 		return $pp->gd()[0]['link'];
