77b84f05d7ba1d7cf4feb19f699a65a031309f34
DuckBurnIncense <DuckBurnIncense@outlook.com>
2022-08-14 15:47:07 +0800

    feat(v): 开发环境title提示

--- short-link/src/main.js
+++ short-link/src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 // fa
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+if (import.meta.env.DEV) document.title = '开发环境 - ' + document.title;
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
