fbfdf5022df6bb766c6a0f3bf9f6057eff8e5ce1
DuckBurnIncense <DuckBurnIncense@outlook.com>
2022-08-15 21:59:59 +0800

    feat(v): 按钮组件

---
+++ short-link/src/components/DButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import { defineEmits } from 'vue';
+	defineEmits([
+		'click'
+	])
+</script>
+
+<template>
+	<button @click="$emit('click', event)"><slot></slot></button>
+</template>
+
+<style scoped>
+	button {
+        background-color: dimgray;
+        padding: 0.8em;
+        border: 0.2px solid gray;
+        border-radius: 0.2em;
+        color: white;
+    }
+</style>
