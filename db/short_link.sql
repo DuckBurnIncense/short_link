f800036512d778d0bc620d499d28dd9c19fa7dc5
DuckBurnIncense <DuckBurnIncense@outlook.com>
2022-08-15 22:07:02 +0800

    docs(p): 新增访问密码功能

--- db/short_link.sql
+++ db/short_link.sql
@@ -4,10 +4,11 @@ CREATE TABLE `short_link` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `suffix` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `link` text COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `views` int(10) unsigned NOT NULL DEFAULT '0',
   `time` int(10) unsigned NOT NULL,
   `ip` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
   `ban` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `suffix` (`suffix`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='链.ml';
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='链.ml'
